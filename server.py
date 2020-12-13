fd43de6a6cd97345eb7fc2f90b014a1a44cb990d
SardarBadarSaghir <dataentrybadar@gmail.com>
2020-12-13 18:14:29 +0500

    Added smtp support

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
+import os
+import smtplib
 from models.blog import Posts
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 posts = Posts()
+OWN_EMAIL = os.environ.get('email')
+OWN_PASSWORD = os.environ.get('password')
+print(OWN_EMAIL, OWN_PASSWORD)
 
 
 @app.route('/')
@@ -12,11 +17,27 @@ def index():
 
 @app.route('/about')
 def about():
+
     return render_template('about.html', title="About")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            email = request.form['email']
+            message = request.form['message']
+            print(name, email, message)
+
+        except:
+            name = request.form['name']
+            email = request.form['email']
+            message = request.form['message']
+            print("Something went wrong")
+        finally:
+            send_email(name=name, email=email, message=message)
+
     return render_template('contact.html', title="Contact")
 
 
@@ -27,6 +48,14 @@ def post(post, id=None):
     print()
 
     return render_template('post.html', title=post, post=posts.get_currnt_post())
+
+
+def send_email(name, email,  message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == '__main__':
